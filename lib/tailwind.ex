7b9e2e523bb734bc1008736d2b4439e4dc02ac30
Chris McCord <chris@chrismccord.com>
2021-12-21 01:06:20 -0500

    Fix amd support

--- lib/tailwind.ex
+++ lib/tailwind.ex
@@ -259,7 +259,7 @@ defmodule Tailwind do
       {{:win32, _}, _arch, 64} -> "windows-x64.exe"
       {{:unix, :darwin}, arch, 64} when arch in ~w(arm aarch64) -> "macos-arm64"
       {{:unix, :darwin}, "x86_64", 64} -> "macos-x64"
-      {{:unix, _osname}, "x86_64", 64} -> "linux-x64"
+      {{:unix, _osname}, arch, 64} when arch in ~w(x86_64 amd64) -> "linux-x64"
       {_os, _arch, _wordsize} -> raise "tailwind is not available for architecture: #{arch_str}"
     end
   end
