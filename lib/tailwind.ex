b4bb5bf75fe72cad8ad6f55a8e522543c2815e00
Max Veytsman <maxim@ontoillogical.com>
2022-11-21 12:09:03 -0500

    Delete binary if overwriting

--- lib/tailwind.ex
+++ lib/tailwind.ex
@@ -229,6 +229,11 @@ defmodule Tailwind do
     tailwind_config_path = Path.expand("assets/tailwind.config.js")
     binary = fetch_body!(url)
     File.mkdir_p!(Path.dirname(bin_path))
+
+    if File.exists?(bin_path) do
+      File.rm!(bin_path)
+    end
+
     File.write!(bin_path, binary, [:binary])
     File.chmod(bin_path, 0o755)
 
