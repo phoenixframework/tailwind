b30c59e8677131fea25331c094842b89df0e3c54
José Valim <jose.valim@dashbit.co>
2023-02-09 12:11:12 +0100

    Depend on inets and ssl

--- mix.exs
+++ mix.exs
@@ -24,17 +24,13 @@ defmodule Tailwind.MixProject do
         source_ref: "v#{@version}",
         extras: ["CHANGELOG.md"]
       ],
-      xref: [
-        exclude: [:httpc, :public_key]
-      ],
       aliases: [test: ["tailwind.install --if-missing", "test"]]
     ]
   end
 
   def application do
     [
-      # inets/ssl may be used by Mix tasks but we should not impose them.
-      extra_applications: [:logger],
+      extra_applications: [:logger, :inets, :ssl],
       mod: {Tailwind, []},
       env: [default: []]
     ]
