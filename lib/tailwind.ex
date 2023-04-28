bc7c29ab43ee534c5bed6b8ec1e21b683fcaa95c
Linus De Meyere <linus.dm@gmail.com>
2023-04-28 20:56:27 +0200

    Ignore Browserslist warning.
    By setting the environment variable `BROWSERSLIST_IGNORE_OLD_DATA`, the
    warning about an outdated browserslist can be silenced.
    
    ```
    Browserslist: caniuse-lite is outdated. Please run:
      npx update-browserslist-db@latest
      Why you should do it regularly: https://github.com/browserslist/update-db#readme
    ```
    
    This features is _not_ documented in the browserslist project. It's implemented
    [here](https://github.com/browserslist/browserslist/blob/main/node.js#L389).

--- lib/tailwind.ex
+++ lib/tailwind.ex
@@ -174,9 +174,13 @@ defmodule Tailwind do
     config = config_for!(profile)
     args = config[:args] || []
 
+    env =
+      Keyword.get(config, :env, %{})
+      |> add_env_variable_to_ignore_browserslist_outdated_warning()
+
     opts = [
       cd: config[:cd] || File.cwd!(),
-      env: config[:env] || %{},
+      env: env,
       into: IO.stream(:stdio, :line),
       stderr_to_stdout: true
     ]
@@ -184,6 +188,10 @@ defmodule Tailwind do
     bin_path()
     |> System.cmd(args ++ extra_args, opts)
     |> elem(1)
+  end
+
+  defp add_env_variable_to_ignore_browserslist_outdated_warning(env) do
+    Enum.into(env, %{"BROWSERSLIST_IGNORE_OLD_DATA" => "1"})
   end
 
   @doc """
